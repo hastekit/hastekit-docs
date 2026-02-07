2c66a8f81d26fb7ef0ee6b03ae845ceecf026c0f
Praveen <praveen@amagi.com>
2026-02-08 02:22:01 +0530

    fix: agent tool

--- examples/agents/8_restate_agent/main.go
+++ examples/agents/8_restate_agent/main.go
@@ -6,8 +6,12 @@ import (
 	"os"
 
 	hastekit "github.com/hastekit/hastekit-sdk-go"
+	"github.com/hastekit/hastekit-sdk-go/pkg/agents"
+	"github.com/hastekit/hastekit-sdk-go/pkg/agents/tools"
 	"github.com/hastekit/hastekit-sdk-go/pkg/gateway"
 	"github.com/hastekit/hastekit-sdk-go/pkg/gateway/llm"
+	"github.com/hastekit/hastekit-sdk-go/pkg/gateway/llm/responses"
+	"github.com/hastekit/hastekit-sdk-go/pkg/utils"
 )
 
 func main() {
@@ -48,10 +52,34 @@ func main() {
 		Instruction: client.Prompt("You are helpful assistant. You are interacting with the user named {{name}}"),
 		LLM:         model,
 		History:     history,
+		Tools: []agents.Tool{
+			tools.NewAgentTool(&responses.ToolUnion{
+				OfFunction: &responses.FunctionTool{
+					Name:        "joke-generator-agent",
+					Description: utils.Ptr("Use to generate jokes"),
+					Parameters: map[string]any{
+						"type":     "object",
+						"required": []string{"message"},
+						"properties": map[string]any{
+							"message": map[string]any{
+								"type":        "string",
+								"description": "Message for the agent",
+							},
+						},
+						"additionalProperties": false,
+					},
+				},
+			}, client.NewRestateAgent(&hastekit.AgentOptions{
+				Name:        "joke-generator",
+				Instruction: client.Prompt("You are helpful assistant."),
+				LLM:         model,
+				History:     client.NewConversationManager(),
+			})),
+		},
 	})
 
-	client.StartRestateService("0.0.0.0", "9081") // Do this on the restate service
-	err = http.ListenAndServe(":8070", client)    // Do this on the application that invokes the restate workflow
+	go client.StartRestateService("0.0.0.0", "9081") // Do this on the restate service
+	err = http.ListenAndServe(":8070", client)       // Do this on the application that invokes the restate workflow
 	if err != nil {
 		log.Fatal(err)
 	}
